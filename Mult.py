621f6cc6242c05e5e89e50a8ab9b4e923c60a938
Christopher Hebert <cph015@latech.edu>
2017-09-27 19:17:48 -0500

    added fact. function

--- Mult.py
+++ Mult.py
@@ -19,3 +19,16 @@ def power(num1, num2):
     for i in range(num2):
         ans = ans * num1
     return ans
+
+def fact(num1):
+    if num1 == 0:
+        return 1
+    
+    ans = num1
+    num1 = num1 - 1
+
+    for i in range(num1):
+        ans = ans * num1
+        num1 = num1 - 1
+    return ans
+
