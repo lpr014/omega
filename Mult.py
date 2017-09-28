47a075be59d8c88ccff67e2327e797914b14c539
Christopher Hebert <cph015@latech.edu>
2017-09-27 19:00:05 -0500

    added power funct

--- Mult.py
+++ Mult.py
@@ -11,5 +11,11 @@ def multiply(num1, num2):
     ans = num1 * num2
     return ans
 
+def power(num1, num2):
+    if num2 == 0:
+        return 1
 
-        
+    ans=1
+    for i in range(num2):
+        ans = ans * num1
+    return ans
