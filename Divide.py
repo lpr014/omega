7df2a29d0673c25560dcafe560623d6612227f15
Lorantz Rogers <lpr014@latech.edu>
2017-09-28 18:24:24 -0500

    Divide and remainder

--- Divide.py
+++ Divide.py
@@ -10,4 +10,14 @@ def divide(num1, num2):
     else:
         ans=num1/num2
         return ans
+# remainder using the % symbol
+def mod(num1, num2):
+    if num2==0:
+        print("CANNOT DIVIDE BY ZERO")
+        return
+    else:
+        ans=num1%num2
+        return ans
+
+
 
