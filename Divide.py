d3fd8a38700a5df5be0ac7e4681aff8a160ddab5
Lorantz Rogers <lpr014@latech.edu>
2017-09-28 18:16:31 -0500

    division function

---
+++ Divide.py
@@ -0,0 +1,13 @@
+# Created by Lorantz on 9/28/17 6:15 PM
+
+
+def divide(num1, num2):
+    
+    # divide by zero error check
+    if num2 == 0:
+        print("DIVIDE BY ZERO ERROR")
+        return
+    else:
+        ans=num1/num2
+        return ans
+
