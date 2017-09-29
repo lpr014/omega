c4bdf1c515293e6bb6c50cfd2687f275d4dbda36
Christopher Hebert <cph015@latech.edu>
2017-09-29 17:26:59 -0500

    fixed

--- Mult.py
+++ Mult.py
@@ -13,14 +13,9 @@ def power(num1, num2):
     ans=1
     for i in range(int(num2)):
         ans = ans * num1
-<<<<<<< HEAD
-    #return ans
-    print(ans)
-=======
     return ans
 
 #factorial
->>>>>>> 2fdd3702f59f1881653c1c82a473625222b2e8d5
 def fact(num1):
     if num1 == 0:
         return 1
@@ -31,11 +26,4 @@ def fact(num1):
     for i in range(num1):
         ans = ans * num1
         num1 = num1 - 1
-<<<<<<< HEAD
-    return ans
-        
-power(2,5)
-=======
     return float(ans)
-
->>>>>>> 2fdd3702f59f1881653c1c82a473625222b2e8d5
