a05227e51ee998f7e2f16a98110736ed631bf4be
Lorantz Rogers <lpr014@latech.edu>
2017-09-29 17:38:52 -0500

    Divide, modulo, integer divide

--- Divide.py
+++ Divide.py
@@ -1,11 +1,12 @@
 # Created by Lorantz on 9/28/17 6:15 PM
+# Last edit by Lorantz on 9/29/17 5:20 PM
 
 
 def divide(num1, num2):
     
     # divide by zero error check
     if num2 == 0:
-        print("DIVIDE BY ZERO ERROR")
+        print("CANNOT DIVIDE BY ZERO")
         return
     else:
         ans=num1/num2
