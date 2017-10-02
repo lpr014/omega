e2299e7de4f7d550902283599c6a080bd9eb19d4
MStevenTowns <MST039@latech.edu>
2017-10-02 18:49:40 -0500

    can run a test

--- Test1.py
+++ Test1.py
@@ -1,24 +1,25 @@
 ## from _____ import the class ##
 
 
-from unittest import TestCase
+import unittest
+from Mult import multiply
 
 class CalculatorTest(unittest.TestCase):
-    
-    def setUp(self):
-
-        self.calc = src.calculator.Calculator()
-    
-    def test_add(self):
-        self.assertEqual(self.calc.add(0,9), 9)
-
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2,3), 6)
-
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(9,3), 3)
-              
-
+        self.assertEqual(multiply(2,3), 6)
+    ''' 
+        def setUp(self):
+            self.calc = src.calculator.Calculator()
+        
+        def test_add(self):
+            self.assertEqual(self.calc.add(0,9), 9)
+    '''
+    
+    '''
+        def test_divide(self):
+            self.assertEqual(self.calc.divide(9,3), 3)
+                  
+    '''
 
 if __name__ == '__main__':
-    main()    
+    unittest.main()    
