5c1fc8680abc715520c64fc1dbc8011bdaa6bada
MStevenTowns <MST039@latech.edu>
2017-10-03 16:57:59 -0500

    added support for negative numbers

--- InputParser.py
+++ InputParser.py
@@ -9,7 +9,6 @@ def inputParser():
             if char.isalpha():
                 return('invalid input character')
             try:
-                print(word)
                 if word.isdigit():
                     inString+=[float(word)]
             except:
@@ -20,10 +19,21 @@ def inputParser():
         inString+=[float(word)]
     except:
         None #None for fact to work
+        
     for i in range(len(inString)-1):
         if inString[i]=='/' and inString[i+1]=='/':
             inString[i]='//'
             inString[i+1]='//'
             inString=inString[:i]+inString[i+1:]
+    if inString[0]=='-':
+        inString[1]*=-1
+        inString=inString[1:]
+        
+    for i in range(2,len(inString)-1):
+        if inString[i]=='-':
+            if type(inString[i-1]) is str and type(inString[i+1]) is float:
+                inString[i+1]*=-1
+                inString=inString[:i]+inString[i+1:]
+            
     return(inString)
-inputParser()
+print(inputParser())
