7416d0dd7e01b16c05a809efc0897d9029353e7c
Yasmine Hines <yhines004@gmail.com>
2017-10-02 19:10:34 -0500

    Fix testcases

--- Test1.py
+++ Test1.py
@@ -6,14 +6,15 @@ from Divide import divide
 
 
 class CalculatorTest(unittest.TestCase):
-    def test_multiply(self):
-        self.assertEqual(multiply(2,3), 6)
     
     def test_add(self):
         self.assertEqual(add(0,9), 9)
     
     def test_sub(self):
         self.assertEqual(sub(5,2), 3)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2,3), 6)
 
     def test_divide(self):
         self.assertEqual(divide(9,3), 3)
