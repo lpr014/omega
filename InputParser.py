c62365ded0553ca35ff45a9cfc37557a21381aaa
MStevenTowns <MST039@latech.edu>
2017-10-05 17:51:15 -0500

    added support for pi() and e()

--- InputParser.py
+++ InputParser.py
@@ -1,59 +1,61 @@
+import string
+
 def inputParser():
     word=''
-    inString=[]
-    string=raw_input('\ninput: ').strip()
-    #Changed to fix History output
-    if string=='history':
-        return string
+    inArr=[]
+    inString=raw_input('\ninput: ').strip()    
 
-    if len(string)==0:
+    if len(inString)==0:
         return 0
-
-    if string[:5]=='sqrt(':
-        inString=['sqrt(']
-        string=string[5:]
-    
-    for char in string:
+        
+    if inString=='history':
+        return inString
+    if inString[:5]=='sqrt(':
+        inArr=['sqrt(']
+        inString=inString[5:]
+    inString=inString.replace('pi()',"3.141592653589793")
+    inString=inString.replace('e()',"2.718281828459045")
+    for char in inString:
         if char.isdigit() or char=='.':
             word+=char
         elif char.isdigit()==False:
             if char.isalpha():
                 return('invalid input')
             try:
-                inString+=[float(word)]
+                inArr+=[float(word)]
             except:
                 pass
             word=''
-            inString+=[char]
+            inArr+=[char]
     try:
-        inString+=[float(word)]
+        inArr+=[float(word)]
     except:
         None #None for fact to work
         
     #test for //
-    for i in range(len(inString)-1):
-        if inString[i]=='/' and inString[i+1]=='/':
-            inString[i]='//'
-            inString[i+1]='//'
-            inString=inString[:i]+inString[i+1:]
-    if inString[0]=='-':
-        inString[1]*=-1
-        inString=inString[1:]
+    for i in range(len(inArr)-1):
+        if inArr[i]=='/' and inArr[i+1]=='/':
+            inArr[i]='//'
+            inArr[i+1]='//'
+            inArr=inArr[:i]+inArr[i+1:]
+    if inArr[0]=='-':
+        inArr[1]*=-1
+        inArr=inArr[1:]
         
     #test for negative numbers
-    for i in range(2,len(inString)-1):
-        if inString[i]=='-':
-            if type(inString[i-1]) is str and type(inString[i+1]) is float:
-                inString[i+1]*=-1
-                inString=inString[:i]+inString[i+1:]
+    for i in range(2,len(inArr)-1):
+        if inArr[i]=='-':
+            if type(inArr[i-1]) is str and type(inArr[i+1]) is float:
+                inArr[i+1]*=-1
+                inArr=inArr[:i]+inArr[i+1:]
     #test for double characters
-    for i in range(len(inString)-1):
-        if type(inString[i])!=float and type(inString[i+1])!=float:
+    for i in range(len(inArr)-1):
+        if type(inArr[i])!=float and type(inArr[i+1])!=float:
             return("invalid input")
             
     #don't allow more than 2 numbers and an operation
-    if len(inString)>3:
+    if len(inArr)>3:
         return("invalid input") 
         
     #return array of inputs
-    return(inString)
+    return(inArr)
